0cee628094ae25c38d6e8fb592ab689cbd5bd8c2
zhengzha <zhengzha>
2009-10-13 21:28:51 +0000

    formalize package

--- Model/Journal.pm
+++ Model/Journal.pm
@@ -3,19 +3,16 @@ use strict;
 use Carp;
 use Data::Dumper;
 use Class::Std;
-
 use Ace;
-
 use Chado::WriteChadoMac;
 
 my %journal            :ATTR( :name<journal>             :default<undef>); 
-my %name               :ATTR( :name<name>                :default<undef>);
+my %uniquename         :ATTR( :name<uniquename>          :default<undef>);
 my %other_name         :ATTR( :name<other_name>          :default<undef>);
 my %previous_name      :ATTR( :name<previous_name>       :default<undef>);
 my %url                :ATTR( :name<url>                 :default<undef>);
-
-my %propertytypes                     :ATTR(                                          :default<{}>);
-
+my %propertytypes      :ATTR(                            :default<{}>);
+my %propery            :ATTR( :name<property>            :default<undef>);
 
 sub BUILD {
     my ($self, $ident, $args) = @_;
@@ -29,32 +26,54 @@ sub BUILD {
 sub read_journal {
     my $self = shift;
     my $journal = $journal{ident $self};
-    $self->set_name($journal->name);
-    $self->set_other_name($journal->Other_name->name) if defined($journal->Other_name);
-    $self->set_previous_name($journal->Previous_name->name) if defined($journal->Previous_name);
-    $self->set_url($journal->URL->name) if defined($journal->URL);
+    $self->set_uniquename($journal->uniquename);
+    my %property;
+    for my $prop (keys %{$propertytypes{ident $self}}) {
+	if (defined($paper->$prop)) {
+	    my @tmp = names_at($paper, $prop);
+	    $property{$propertytypes{ident $self}->{$prop}} = \@tmp;	    
+	}
+    }
+    $self->set_property(\%property);	
 }
 
 sub write_journal_pub {
     my $self = shift;
     my $doc = shift;
     my %arg = @_;
+    my @pub;
     my $pub_el = create_ch_pub(doc => $doc,
-			       uniquename => $self->get_name,
-			       title => $self->get_name,
+			       uniquename => $self->get_uniquename,
+			       title => $self->get_uniquename,
+			       macro_id => $self->get_uniquename,
 			       %arg);
+    push @pub, $pub_el;
 
-    for my $tag (keys %{$types{ident $self}}) {
+    if (%{$self->get_property()}) {
+	while (my ($type, $p) = each %{$self->get_property()}) {
+	    for my $value (@$p) {
+		next if $value eq $self->get_uniquename && $type eq 'pubmed_abstract'; #junk info
+		my $pp_el = create_ch_pubprop(doc => $doc,
+					      pub_id => $self->get_uniquename,
+					      type => $type,
+					      value => $value);     
+		push @pub, $pp_el;
+	    }
+	}
+    }
+
+
+    for my $tag (keys %{$propertytypes{ident $self}}) {
 	my $func = 'get_' . $tag;
 	my $value = $self->$func;
 	if (defined($value)) {
-	    my $pubprop_el = create_ch_pubprop(doc => $doc,
-					       value => $value,
-					       type => $types{ident $self}->{$tag});
-	    $pub_el->appendChild($pubprop_el);
+	    my $pp_el = create_ch_pubprop(doc => $doc,
+					  value => $value,
+					  type => $propertytypes{ident $self}->{$tag});
+	    push @pub, $pp_el;
 	}
     }
-    return $pub_el;
+    return @pub;
 }
 
 1;
