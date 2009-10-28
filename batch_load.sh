b489859cc49f0b1cca1456bd16ffdc518175f540
zhengzha <zhengzha>
2009-10-28 15:58:00 +0000

    shell script to load xml to chado using stag-storenode.pl

---
+++ batch_load.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+for xmlfile in `find /home/zheng/migrate/xmldump -maxdepth 1 -name "*.xml"`
+do
+  stag-storenode.pl -d 'dbi:Pg:dbname=ws190;host=localhost;port=5432' --user zheng --password weigaocn $xmlfile;
+done
